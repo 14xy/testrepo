54ab3d78c42befec0f59f52e7fc5f25b1f321a7c
14xy <65587473+14xy@users.noreply.github.com>
2021-01-18 17:38:16 +0800

    Add files via upload

---
+++ jackpot game.py
@@ -0,0 +1,19 @@
+import random
+
+jackpot = random.randint(1,10)
+
+hit = False
+for i in range(1,3):
+    guess = int(input("Enter your guess: "))
+    if guess > jackpot:
+        print("Your guess is too high!")
+    elif guess < jackpot:
+        print("Your guess is too low!")
+    else:
+        hit = True
+        break
+
+if hit == False:
+    print("You lose. The jackpot is",jackpot)
+else:
+    print("You hit the JACKPOT!")
